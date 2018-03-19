92a6ac715ea299acf2ea200204322f3e7115fddd
stalary <452024236@qq.com>
2018-03-19 11:44:44 +0800

    ADD：树结点

---
+++ src/TreeNode.java
@@ -0,0 +1,15 @@
+/**
+ * TreeNode
+ *
+ * @author lirongqian
+ * @since 2018/03/19
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
