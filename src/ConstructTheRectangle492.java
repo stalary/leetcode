8c956d3dda8ceb6b314e1c7fa4180a618b5ff053
stalary <452024236@qq.com>
2018-03-26 17:22:57 +0800

    ADD： 求一个数字由两个数字相乘的数组，并且两个数字是最接近的

---
+++ src/ConstructTheRectangle492.java
@@ -0,0 +1,34 @@
+
+/**
+ * ConstructTheRectangle492
+ * <p>
+ * 求一个数字由两个数字相乘的数组，并且两个数字是最接近的
+ *
+ * @author lirongqian
+ * @since 2018/03/26
+ */
+public class ConstructTheRectangle492 {
+
+    public static void main(String[] args) {
+        System.out.println(constructRectangle(10000000)[0]);
+        System.out.println(constructRectangle(10000000)[1]);
+    }
+
+    /**
+     * 可以首先先对area进行开方，从而缩小范围
+     * 然后递增开放后的数，直到相乘为area
+     * @param area
+     * @return
+     */
+    public static int[] constructRectangle(int area) {
+        int[] a = new int[2];
+        int a1 = (int) Math.sqrt(area);
+        while (area % a1 != 0) {
+            a1++;
+        }
+        a[0] = Math.max(a1, area / a1);
+        a[1] = Math.min(a1, area / a1);
+        return a;
+    }
+
+}
