35f2c294d843592ad286fedeec7916aa099b65e0
stalary <452024236@qq.com>
2018-03-26 11:13:15 +0800

    ADD： 输入一个正数，转化为二进制，判断是否为跳跃式

---
+++ src/BinaryNumberWithAlternatingBits693.java
@@ -0,0 +1,25 @@
+/**
+ * BinaryNumberWithAlternatingBits693
+ * <p>
+ * 输入一个正数，转化为二进制，判断是否为跳跃式
+ *
+ * @author lirongqian
+ * @since 2017/11/22
+ */
+public class BinaryNumberWithAlternatingBits693 {
+
+    public static void main(String[] args) {
+        System.out.println(hasAlternatingBits(5));
+    }
+
+    public static boolean hasAlternatingBits(int n) {
+        String str = Integer.toBinaryString(n);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
