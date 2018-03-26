3965730a9824e99ef12d88a4b0dc0a970f799052
stalary <452024236@qq.com>
2018-03-26 17:37:07 +0800

    ADD：将一个排好序的数组转化为一颗二叉搜索树

---
+++ src/ConvertSortedArrayToBinarySearchTree108.java
@@ -0,0 +1,53 @@
+
+/**
+ * ConvertSortedArrayToBinarySearchTree108
+ *
+ * 将一个排好序的数组转化为一颗二叉搜索树
+ * @author lirongqian
+ * @since 2018/03/26
+ */
+public class ConvertSortedArrayToBinarySearchTree108 {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{
+                -10, -3, 0, 5, 9
+        };
+        TreeNode node = sortedArrayToBST(nums);
+        printTree(node);
+    }
+
+    public static void printTree(TreeNode node) {
+        if (node != null) {
+            System.out.println(node.val);
+            printTree(node.left);
+            printTree(node.right);
+        }
+    }
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        return help(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * 每次取中点，代表根节点，然后依次取左儿子和右儿子进行赋值
+     * @param nums
+     * @param low
+     * @param high
+     * @return
+     */
+    public static TreeNode help(int[] nums, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+        int mid = (high + low) / 2;
+        // 将中点作为根
+        TreeNode node = new TreeNode(nums[mid]);
+        // 分别构造左右结点
+        node.left = help(nums, low, mid - 1);
+        node.right = help(nums, mid + 1, high);
+        return node;
+    }
+}
