66f51b55517ffb4340e59051e407cc5a7ab8942c
stalary <452024236@qq.com>
2018-03-28 14:14:55 +0800

    ADD：找出每一层的最大值

---
+++ src/FindLargestValueInEachTreeRow515.java
@@ -0,0 +1,35 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FindLargestValueInEachTreeRow515
+ *
+ * 找出每一层的最大值
+ * @author lirongqian
+ * @since 2018/03/27
+ */
+public class FindLargestValueInEachTreeRow515 {
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res, 0);
+        return res;
+    }
+
+    private void helper(TreeNode root, List<Integer> res, int d){
+        if(root == null){
+            return;
+        }
+        //扩展list
+        if(d == res.size()){
+            res.add(root.val);
+        } else {
+            // 向当前层存入最大值，此时，当前层以及至少存在一个元素
+            res.set(d, Math.max(res.get(d), root.val));
+        }
+        // 递归查找左右子结点
+        helper(root.left, res, d+1);
+        helper(root.right, res, d+1);
+    }
+}
