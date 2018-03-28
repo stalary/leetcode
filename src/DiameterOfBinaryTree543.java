372ad42c0edff7ce6b1f86acabe79a5bd48ed644
stalary <452024236@qq.com>
2018-03-28 14:12:19 +0800

    ADD：求二叉树的最长路径

---
+++ src/DiameterOfBinaryTree543.java
@@ -0,0 +1,39 @@
+/**
+ * DiameterOfBinaryTree543
+ *
+ * 求二叉树的最长路径
+ * 思路：
+ * 递归的遍历二叉树的左儿子和右儿子，并对路径求和，每次求最大的路径
+ * @author lirongqian
+ * @since 2018/03/27
+ */
+public class DiameterOfBinaryTree543 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        System.out.println(diameterOfBinaryTree(root));
+    }
+
+    static int max = 0;
+    public static int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        // 递归查找左右结点的最大深度
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        // 求出最大值
+        max = Math.max(max, left + right);
+        // 返回左右较大的值，并加一层深度
+        return Math.max(left, right) + 1;
+    }
+}
