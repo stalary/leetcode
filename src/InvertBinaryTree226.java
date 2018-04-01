309e1dbafe9b81a35de9b9d783e7a2cd86ef3bc5
stalary <452024236@qq.com>
2018-04-01 23:16:04 +0800

    ADD：交换二叉树的左右结点

---
+++ src/InvertBinaryTree226.java
@@ -0,0 +1,40 @@
+/**
+ * InvertBinaryTree226
+ *
+ * 交换二叉树的左右结点
+ *
+ * Invert a binary tree.
+
+        4
+      /   \
+     2     7
+    / \   / \
+   1   3 6   9
+ to
+
+        4
+      /   \
+     7     2
+    / \   / \
+   9   6 3   1
+ * @author lirongqian
+ * @since 2018/04/01
+ */
+public class InvertBinaryTree226 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        // 递归的交换左右的结点
+        TreeNode node = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(node);
+        return root;
+    }
+
+}
