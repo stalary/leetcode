94fb651a6e848438060f284b21b2865de41eefd7
stalary <452024236@qq.com>
2018-04-07 17:30:31 +0800

    ADD：求出到达终点的路径数量

---
+++ src/UniquePaths62.java
@@ -0,0 +1,31 @@
+/**
+ * UniquePaths62
+ * 求出到达终点的路径数量
+ *
+ * @author lirongqian
+ * @since 2018/04/07
+ */
+public class UniquePaths62 {
+
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+        int[][] map = new int[m][n];
+        // 为横轴赋值
+        for (int i = 0; i < m; i++) {
+            map[i][0] = 1;
+        }
+        // 为纵轴赋值
+        for (int j = 0; j < n; j++) {
+            map[0][j] = 1;
+        }
+        // 向下和向右移动
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                map[i][j] = map[i - 1][j] + map[i][j - 1];
+            }
+        }
+        return map[m - 1][n - 1];
+    }
+}
