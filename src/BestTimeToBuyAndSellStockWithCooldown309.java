d9a9eae81f8c6de32c422ae5bc68680d2fefa7af
stalary <452024236@qq.com>
2018-04-09 09:46:53 +0800

    ADD：买卖股票的最好时机309

--- src/BestTimeToBuyAndSellStockWithCooldown309.java
+++ src/BestTimeToBuyAndSellStockWithCooldown309.java
@@ -12,13 +12,24 @@ public class BestTimeToBuyAndSellStockWithCooldown309 {
 
     }
 
+    /**
+     * A further observation is that and rest[i] <= sell[i] is also true therefore
+     *
+     * rest[i] = sell[i-1]
+     * Substitute this in to buy[i] we now have 2 functions instead of 3:
+     *
+     * buy[i] = max(sell[i-2]-price, buy[i-1])
+     * sell[i] = max(buy[i-1]+price, sell[i-1])
+     * @param prices
+     * @return
+     */
     public int maxProfit(int[] prices) {
         // 买入利润需要首先设置最小值，防止亏损时的计算错误
         int sell = 0, prevSell = 0, buy = Integer.MIN_VALUE, prevBuy;
         for (int price : prices) {
             // 上一次购买的最大利润
             prevBuy = buy;
-            // 比较本次买入和上一次买入的最大利润
+            // 比较本次买入和上上次买入的最大利润
             buy = Math.max(prevSell - price, prevBuy);
             // 上一次出售的最大利润
             prevSell = sell;
