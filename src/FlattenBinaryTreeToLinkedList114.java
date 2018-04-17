6a183ebc5a5553e356e46a03262466cd4548e81e
stalary <452024236@qq.com>
2018-04-17 23:01:36 +0800

    ADD：将二叉树转为只有右节点的二叉树

---
+++ src/FlattenBinaryTreeToLinkedList114.java
@@ -0,0 +1,31 @@
+/**
+ * FlattenBinaryTreeToLinkedList114
+ * 将二叉树转为只有右节点的二叉树
+ * @author lirongqian
+ * @since 2018/04/16
+ */
+public class FlattenBinaryTreeToLinkedList114 {
+
+    public void flatten(TreeNode root) {
+        flatten(root, null);
+    }
+
+    /**
+     * 首先获取最右节点
+     * 然后依次将当前节点放到父节点的右侧，并且清空左结点
+     * @param root
+     * @param pre
+     * @return
+     */
+    private TreeNode flatten(TreeNode root, TreeNode pre) {
+        if (root == null) {
+            return pre;
+        }
+        pre = flatten(root.right, pre);
+        pre = flatten(root.left, pre);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+        return pre;
+    }
+}
