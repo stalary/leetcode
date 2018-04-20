713bed55dc3ccb261099bf4c748ffc5818367445
stalary <452024236@qq.com>
2018-04-21 00:13:49 +0800

    ADD：序列化反序列化二叉树

---
+++ src/SerializeAndDeserializeBinaryTree297.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+/**
+ * SerializeAndDeserializeBinaryTree297
+ * 序列化反序列化二叉树
+ * @author lirongqian
+ * @since 2018/04/18
+ */
+public class SerializeAndDeserializeBinaryTree297 {
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(3);
+        node.right.left = new TreeNode(4);
+        node.right.right = new TreeNode(5);
+        System.out.println(new SerializeAndDeserializeBinaryTree297().serialize(node));
+    }
+
+    private static final String spliter = ",";
+    private static final String NN = "X";
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        buildString(root, sb);
+        return sb.toString();
+    }
+
+    private void buildString(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append(NN).append(spliter);
+        } else {
+            sb.append(node.val).append(spliter);
+            buildString(node.left, sb);
+            buildString(node.right,sb);
+        }
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Deque<String> nodes = new LinkedList<>();
+        nodes.addAll(Arrays.asList(data.split(spliter)));
+        return buildTree(nodes);
+    }
+
+    private TreeNode buildTree(Deque<String> nodes) {
+        String val = nodes.remove();
+        if (val.equals(NN)) {
+            return null;
+        } else {
+            TreeNode node = new TreeNode(Integer.valueOf(val));
+            node.left = buildTree(nodes);
+            node.right = buildTree(nodes);
+            return node;
+        }
+    }
+}
