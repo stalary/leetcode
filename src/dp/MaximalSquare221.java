9632916aaa1c34671a54e0f9aa4ae7a4ad50f4d3
stalary <452024236@qq.com>
2018-05-04 16:54:05 +0800

    ADD：求1的最大面积

---
+++ src/dp/MaximalSquare221.java
@@ -0,0 +1,29 @@
+package dp;
+
+/**
+ * dp.MaximalSquare221
+ * 求1的最大面积
+ *
+ * @author lirongqian
+ * @since 2018/05/04
+ */
+public class MaximalSquare221 {
+
+    public int maximalSquare(char[][] a) {
+        if (a.length == 0) {
+            return 0;
+        }
+        int m = a.length, n = a[0].length, result = 0;
+        int[][] b = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a[i - 1][j - 1] == '1') {
+                    // 获取三个周围点的最小值
+                    b[i][j] = Math.min(Math.min(b[i][j - 1], b[i - 1][j - 1]), b[i - 1][j]) + 1;
+                    result = Math.max(b[i][j], result); // update result
+                }
+            }
+        }
+        return result * result;
+    }
+}
