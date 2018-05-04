c39bebece984abb07b50505adce94b393b6c1455
stalary <452024236@qq.com>
2018-05-04 16:53:57 +0800

    ADD：找到环形list的入口，如果没有环返回null

--- src/LinkedListCycleII142.java
+++ src/LinkedListCycleII142.java
@@ -1,56 +1,44 @@
 /**
  * LinkedListCycleII142
  * 找到环形list的入口，如果没有环返回null
+ *
  * @author lirongqian
  * @since 2018/05/04
  */
 public class LinkedListCycleII142 {
 
-    public ListNode detectCycle(ListNode head) {
-        ListNode meetNode = meetNode(head);
-        if (meetNode == null) {
-            return null;
-        }
-        // 统计环内元素数量
-        int count = 1;
-        ListNode p1 = meetNode;
-        while (p1.next != meetNode) {
-            p1 = p1.next;
-            count++;
-        }
-        p1 = head;
-        for (int i = 0; i < count; i++) {
-            p1 = p1.next;
-        }
-        ListNode p2 = head;
-        while (p1 != p2) {
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-        return p1;
-    }
-
     /**
-     * 使用快慢指针查找相遇点
+     * 首先使用快慢指针，判断是否有环
+     *
      * @param head
      * @return
      */
-    public ListNode meetNode(ListNode head) {
-        if (head == null || head.next == null) {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
             return null;
         }
-        ListNode slow = head.next;
-        ListNode fast = slow.next;
+        ListNode slow = head;
+        ListNode fast = head;
         while (slow != null && fast != null) {
-            if (slow == fast) {
-                return fast;
-            }
             slow = slow.next;
             fast = fast.next;
-            if (slow != fast) {
+            if (fast != null) {
                 fast = fast.next;
             }
+            if (slow == fast) {
+                break;
+            }
         }
-        return null;
+        // 存在null时代表无环
+        if (slow == null || fast == null) {
+            return null;
+        }
+        // 将fast指针重置，当与相遇点再次相遇时
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
