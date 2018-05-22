d9f743ffad8c51acac8abb17c36f1ce1740466fb
stalary <452024236@qq.com>
2018-05-22 20:43:28 +0800

    ADD: 求二叉树最大深度

---
+++ src/MaximumDepthOfBinaryTree104.java
@@ -0,0 +1,23 @@
+
+/**
+ * MaximumDepthOfBinaryTree104
+ * 求二叉树最大深度
+ * @author lirongqian
+ * @since 2018/05/22
+ */
+public class MaximumDepthOfBinaryTree104 {
+
+    /**
+     * 去最大深度，递归查找左右子树即可，每次+1
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
