7d56fcb686a4170501e61c8205f93632700ea14e
stalary <452024236@qq.com>
2018-07-29 23:21:40 +0800

    求右边当前数字小的数量

--- src/CountOfSmallerNumbersAfterSelf315.java
+++ src/CountOfSmallerNumbersAfterSelf315.java
@@ -1,49 +1,56 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * CountOfSmallerNumbersAfterSelf315
  * 求右边当前数字小的数量
+ *
  * @author lirongqian
  * @since 2018/04/18
  */
 public class CountOfSmallerNumbersAfterSelf315 {
 
     public static void main(String[] args) {
-        System.out.println(new CountOfSmallerNumbersAfterSelf315().countSmaller(new int[] {
+        System.out.println(new CountOfSmallerNumbersAfterSelf315().countSmaller(new int[]{
                 5, 2, 6, 1
         }));
     }
 
-    class Node {
-        Node left, right;
-        int val, sum, dup = 1;
-        public Node(int v, int s) {
-            val = v;
-            sum = s;
-        }
-    }
     public List<Integer> countSmaller(int[] nums) {
         Integer[] ans = new Integer[nums.length];
-        Node root = null;
+        List<Integer> sorted = new ArrayList<>();
         for (int i = nums.length - 1; i >= 0; i--) {
-            root = insert(nums[i], root, ans, i, 0);
+            int index = findIndex(sorted, nums[i]);
+            ans[i] = index;
+            sorted.add(index, nums[i]);
         }
         return Arrays.asList(ans);
     }
-    private Node insert(int num, Node node, Integer[] ans, int i, int preSum) {
-        if (node == null) {
-            node = new Node(num, 0);
-            ans[i] = preSum;
-        } else if (node.val == num) {
-            node.dup++;
-            ans[i] = preSum + node.sum;
-        } else if (node.val > num) {
-            node.sum++;
-            node.left = insert(num, node.left, ans, i, preSum);
-        } else {
-            node.right = insert(num, node.right, ans, i, preSum + node.dup + node.sum);
+
+    private int findIndex(List<Integer> sorted, int target) {
+        if (sorted.size() == 0) {
+            return 0;
         }
-        return node;
+        int start = 0;
+        int end = sorted.size() - 1;
+        if (sorted.get(end) < target) {
+            return end + 1;
+        }
+        if (sorted.get(start) >= target) {
+            return 0;
+        }
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (sorted.get(mid) < target) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        if (sorted.get(start) >= target) {
+            return start;
+        }
+        return end;
     }
 }
