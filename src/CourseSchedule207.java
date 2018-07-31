82b8dceaac84d555a1882f6707c8b448085d07f2
stalary <452024236@qq.com>
2018-07-31 17:36:34 +0800

    课程表，求课程表是否可能

--- src/CourseSchedule207.java
+++ src/CourseSchedule207.java
@@ -1,55 +1,71 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
  * CourseSchedule207
- * 求图是否有环
+ * 课程表，求课程表是否可能
  *
  * @author lirongqian
  * @since 2018/04/29
  */
 public class CourseSchedule207 {
 
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // 构建图
-        ArrayList[] graph = new ArrayList[numCourses];
-        int[] degree = new int[numCourses];
+    public static void main(String[] args) {
+        System.out.println(canFinish(3, new int[][] {
+                {1, 0}, {2, 1}
+        }));
+    }
+
+    /**
+     * Input: 2, [[1,0]]
+     * Output: true
+     * Explanation: 完成1之前需要完成0
+     *
+     * Input: 2, [[1,0],[0,1]]
+     * Output: false
+     * Explanation: 完成1之前要完成0，完成0之前要完成1所以不可能实现
+     *
+     * 判断所有的
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (numCourses <= 0) {
+            return false;
+        }
         Queue<Integer> queue = new LinkedList<>();
-        int count = 0;
-        // 初始化图
-        for (int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList();
+        int[] inDegree = new int[numCourses];
+        // 构建入边
+        for (int i = 0; i < prerequisites.length; i++) {
+            // 前置条件
+            inDegree[prerequisites[i][1]]++;
         }
-        // 将出现的课程添加
-        for (int[] prerequisite : prerequisites) {
-            degree[prerequisite[1]]++;
-            graph[prerequisite[0]].add(prerequisite[1]);
-        }
-        // 将入度为0的添加
-        for (int i = 0; i < degree.length; i++) {
-            if (degree[i] == 0) {
-                queue.add(i);
-                count++;
+        for (int i = 0; i < inDegree.length; i++) {
+            // 不存在前置时直接存入队列，用于消除边
+            if (inDegree[i] == 0) {
+                queue.offer(i);
             }
         }
-        // 计算出度
-        while (queue.size() != 0) {
-            int course = queue.poll();
-            for (int i = 0; i < graph[course].size(); i++) {
-                int pointer = (int) graph[course].get(i);
-                degree[pointer]--;
-                if (degree[pointer] == 0) {
-                    queue.add(pointer);
-                    count++;
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (x == prerequisites[i][0]) {
+                    // 找到前置时进行--
+                    inDegree[prerequisites[i][1]]--;
+                    // 当边为0时，加入任务，继续进行入边判断
+                    if (inDegree[prerequisites[i][1]] == 0) {
+                        queue.offer(prerequisites[i][1]);
+                    }
                 }
             }
         }
-        // count等于课程数量时返回true
-        if (count == numCourses) {
-            return true;
-        } else {
-            return false;
+        for (int i = 0; i < inDegree.length; i++) {
+            // 当仍然存在边时即不满足条件
+            if (inDegree[i] != 0) {
+                return false;
+            }
         }
+        return true;
     }
 }
