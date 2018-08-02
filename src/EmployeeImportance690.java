42b355172160374feda467f69a0f7c501ce8d905
stalary <452024236@qq.com>
2018-08-02 20:44:35 +0800

    首先选出一个领导者，然后统计他以及所有下属的importance，下属的下属仍然属于领导者的下属

--- src/EmployeeImportance690.java
+++ src/EmployeeImportance690.java
@@ -22,10 +22,22 @@ public class EmployeeImportance690 {
         System.out.println(getImportance1(employees, 1));
     }
 
+    /**
+     * Input: [[1, 5, [2, 3]], [2, 3, []], [3, 3, []]], 1
+     * Output: 11
+     * Explanation:
+     * Employee 1 has importance value 5, and he has two direct subordinates:
+     * employee 2 and employee 3. They both have importance value 3.
+     * So the total importance value of employee 1 is 5 + 3 + 3 = 11.
+     */
     static int total = 0;
     public static int getImportance(List<Employee> employees, int id) {
         // 找出领导者
-        Employee manager = employees.stream().filter(e -> e.id == id).collect(Collectors.toList()).get(0);
+        Employee manager = employees
+                .stream()
+                .filter(e -> e.id == id)
+                .collect(Collectors.toList())
+                .get(0);
         total += manager.importance;
         // 递归算出所有下级的importance
         manager.subordinates.forEach(subId -> getImportance(employees, subId));
