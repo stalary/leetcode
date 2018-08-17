58bee2e340cb24585bd35a5cce53ff62b468b79e
stalary <452024236@qq.com>
2018-08-17 09:52:18 +0800

    找出每一层的最大值

--- src/binarytree/FindLargestValueInEachTreeRow515.java
+++ src/binarytree/FindLargestValueInEachTreeRow515.java
@@ -1,9 +1,10 @@
+package binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * FindLargestValueInEachTreeRow515
+ * binarytree.FindLargestValueInEachTreeRow515
  * <p>
  * 找出每一层的最大值
  *
@@ -26,7 +27,7 @@ public class FindLargestValueInEachTreeRow515 {
         if (d == res.size()) {
             res.add(root.val);
         } else {
-            // 向当前层存入最大值，此时，当前层以及至少存在一个元素
+            // 向当前层存入最大值，此时，当前层已经至少存在一个元素
             res.set(d, Math.max(res.get(d), root.val));
         }
         // 递归查找左右子结点
