bed0cd7333b2ec45f81afae759484f600b6ac36f
stalary <452024236@qq.com>
2018-08-19 15:17:54 +0800

    反转链表

--- src/ReverseLinkedList206.java
+++ src/ReverseLinkedList206.java
@@ -11,9 +11,13 @@ public class ReverseLinkedList206 {
     public ListNode reverseList(ListNode head) {
         ListNode pre = null;
         while (head != null) {
+            // 保留下一个节点
             ListNode next = head.next;
+            // 下一个节点为前一个节点
             head.next = pre;
+            // 当前节点为下一次的前一个节点
             pre = head;
+            // 将当前节点后移
             head = next;
         }
         return pre;
